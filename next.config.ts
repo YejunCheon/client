158633fe78831afc9572cfd4882193cabc237a3d
천예준 <yje10007@u.sogang.ac.kr>
2025-11-06 00:07:42 +0900

    refactor: API URL 설정 및 리라이트 기능 추가
    - next.config.ts: API 리라이트 설정 추가로 외부 API 서버로의 요청 경로 지정
    - config.ts: 브라우저와 서버 환경에 따라 API URL을 동적으로 설정하는 함수 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    const apiServer =
+      process.env.NEXT_PUBLIC_API_URL ||
+      'http://dealchain-env.eba-tpa3rca3.ap-northeast-2.elasticbeanstalk.com';
+
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiServer}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
